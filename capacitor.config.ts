93b3819788c097c173070c881cddac1e9f93b187
Stefano <s.macinaleone@gmail.com>
2023-11-14 18:38:38 +0100

    updated : app name

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'newProject',
+  appName: 'movies',
   webDir: 'www',
   server: {
     androidScheme: 'https'
